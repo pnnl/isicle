0eebe5a6b7306bf3e969a3e607d1823ac77bf655
jessbade <jessicabade01@gmail.com>
2018-07-10 09:59:13 -0700

    update, looks to config.yaml
    Looks to config.yaml or --output for override default output location.

--- generate_input.py
+++ generate_input.py
@@ -1,34 +1,33 @@
 from argparse import ArgumentParser, FileType
 from pandas import DataFrame, read_table
 from chembl_ikey import inchi_to_inchikey
-# chembl_ikey opensource github
-# file ikey.py slightly modified, added to line 27: bStdFormat = 0
-import os
+from os import path
 from pathlib import Path
+import yaml
 
 # Run file from command line with location of inchi source files, optional naming
 #   ex: python generate_input.py ./processed_InChI.txt
 #           default file output ./input/inchikey.inchi
-#   or: python generate_input.py ./processed_InChI.txt --unique
-#           to write unique filepaths
-
-parser = ArgumentParser()
-parser.add_argument('filepath', type=FileType('r'))
-parser.add_argument('--unique', nargs='?')
-args = parser.parse_args()
-df = read_table(args.filepath, sep='\n', header=None)
-df = df.replace('"', '')
+#   or: python generate_input.py ./processed_InChI.txt --unique --output
+#           to write unique filepaths and a unique output path
 
 
 def main():
-    unique_inchis() if args.unique else default_inchis()
+    if args.output is not None:
+        config = args.output
+    else:
+        stream = open('config.yaml', 'r')
+        configuration = yaml.load(stream)
+        config = path.join(configuration['path'], './input/')
+        stream.close()
+    unique_inchis() if args.unique else default_inchis(config)
 
 
-def default_inchis():
+def default_inchis(config):
     i = 0
     for row in df.values:
         key = inchi_to_inchikey(row[0])
-        filename = "./input/%s.inchi" % key
+        filename = path.normpath(path.join(config, '%s.inchi' % key))
         checkingfile(i, filename)
         i += 1
 
@@ -37,7 +36,7 @@ def unique_inchis():
     i = 0
     for row in df.values:
         key = inchi_to_inchikey(row[0])
-        filename = os.path.normpath(input("Write filepath including .inchi for %s \n" % key))
+        filename = os.path.normpath(input("Type filepath including .inchi for %s \n" % key))
         checkingfile(i, filename)
         i += 1
 
@@ -54,13 +53,20 @@ def checkingfile(i, filename):
         if overwrite == 'y':
             df.iloc[i].to_csv(filename, index=False, sep="\t", header=None)
         elif overwrite == 'n':
-            new_filename = os.path.normpath(input('Type new filename.\n'))
+            new_filename = os.path.normpath(input("Type new filepath, including .inchi\n"))
             checkingfile(i, new_filename)
         else:
             print("Invalid. File not saved.\n")
 
 
 if __name__ == '__main__':
+    parser = ArgumentParser(description="A program to convert a file of inchis to individual inchikey.inchi files.")
+    parser.add_argument('filepath', type=FileType('r'), help="Include valid filepath to inchi file.")
+    parser.add_argument('--unique', nargs='?', const='store_true', help="Include unique to write uniquefilename.inchi")
+    parser.add_argument('--output', nargs='?', const=None,
+                        help="Allows for unique inchi output location (Default path: config.yaml[''path'']/input/")
+    args = parser.parse_args()
+    df = read_table(args.filepath, sep='\n', header=None)
+    df = df.replace('"', '')
     main()
-
-parser.exit()
+    parser.exit()
