563e6ba313ba40d17e01cec7b1bbcbcb001bd92a
Sean Colby <seanmcolby@gmail.com>
2018-10-05 11:27:25 -0700

    removes extra slash

--- isicle/resources/nwchem/constance.sh
+++ isicle/resources/nwchem/constance.sh
@@ -6,8 +6,8 @@ module load intel/14.0.2
 module load intelmpi
 
 export ARMCI_DEFAULT_SHMMAX=32768
-export NWCHEM_BASIS_LIBRARY="/people/scicons/apps/nwchem-6.6//src/basis/libraries/"
-export NWCHEM_NWPW_LIBRARY="/people/scicons/apps/nwchem-6.6//src/nwpw/libraryps/"
+export NWCHEM_BASIS_LIBRARY="/people/scicons/apps/nwchem-6.6/src/basis/libraries/"
+export NWCHEM_NWPW_LIBRARY="/people/scicons/apps/nwchem-6.6/src/nwpw/libraryps/"
 # this disables xeon phi offload
 export NWC_RANKS_PER_DEVICE=0
 # this disables threaded in MKL since it is better to keep it to advanced users
