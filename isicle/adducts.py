c153a790afc2b605ee05f04e816da6296cad2427
Sean Colby <seanmcolby@gmail.com>
2021-01-26 13:18:43 -0800

    Enforce PEP8 adherence

--- isicle/adducts.py
+++ isicle/adducts.py
@@ -1,3 +1,4 @@
+from isicle.interfaces import AdductInterface
 
 
 class Adduct(AdductInterface):
@@ -14,31 +15,30 @@ class Adduct(AdductInterface):
         """
         Parses in adduct ions to be generated
         """
-        #parse in ions from file
-        #self.ions =
+        # parse in ions from file
+        # self.ions =
 
     def generate_adducts(self):
         """
         Calls different generation methods according to desired ion reaction.
         """
-        #TODO check if desired adduct can be formed
-        check_valid()
-        #TODO institute iterative approach to complex adduct formation
-        #consult https://github.com/grimme-lab/crest/issues/2 for supported ions by xtb CREST
-        positive_mode()
-        negative_mode()
-        #TODO institute manual generation capabilities of output molecule
-        #default positive_mode/negative_mode to xtb CREST, alt for manual generation
+        # TODO check if desired adduct can be formed
+        self.check_valid()
+        # TODO institute iterative approach to complex adduct formation
+        # consult https://github.com/grimme-lab/crest/issues/2 for supported ions by xtb CREST
+        self.positive_mode()
+        self.negative_mode()
+        # TODO institute manual generation capabilities of output molecule
+        # default positive_mode/negative_mode to xtb CREST, alt for manual generation
 
     def check_valid(self):
         """
         Checks if proposed adduct can be formed.
         """
-        #consider calc_new_mz from
-        #https://github.com/pnnl/mame/blob/24f9fcb19639d5a1c2ca0f788c55d6a2efe18ca6/mame/mame_utils.py
-        #consider add_formala, remove_formula from
-        #https://github.com/pnnl/mame/blob/24f9fcb19639d5a1c2ca0f788c55d6a2efe18ca6/mame/formula_module.py
-
+        # consider calc_new_mz from
+        # https://github.com/pnnl/mame/blob/24f9fcb19639d5a1c2ca0f788c55d6a2efe18ca6/mame/mame_utils.py
+        # consider add_formala, remove_formula from
+        # https://github.com/pnnl/mame/blob/24f9fcb19639d5a1c2ca0f788c55d6a2efe18ca6/mame/formula_module.py
 
     def save(self):
         """
@@ -59,7 +59,7 @@ class Adduct(AdductInterface):
         STR must be string of element and charge.
         [Na+]: (ex.) crest 'struc.xyz' -protonate -swel 'Na+'
         """
-        #consider -twel for tautomeric forms of output adduct
+        # consider -twel for tautomeric forms of output adduct
 
     def negative_mode(self):
         """
@@ -70,5 +70,5 @@ class Adduct(AdductInterface):
         Default:
         [H-]: (ex.) crest 'struc.xyz' -deprotonate
         """
-        #consider -twel for tautomeric forms of output adduct
-        #call
+        # consider -twel for tautomeric forms of output adduct
+        # call
