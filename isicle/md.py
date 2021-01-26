38619fe67b5b645a1b6b7fdbfd8b9f4443bd896c
Sean Colby <seanmcolby@gmail.com>
2021-01-26 13:25:24 -0800

    Consolidate `mtd` module into `md` module

--- isicle/md.py
+++ isicle/md.py
@@ -1,8 +1,24 @@
 
 
+def xtb(input, method='gff', optlevel='Normal'):
+    '''
+    Geometry input can be TURBOMOLE coordinates (.coord/.tmol), any valid Xmol (e.g. .xyz),
+    mol files (.mol), Structure-Data files (.sdf), Protein Database Files (.pdb),
+    Vasp’s POSCAR and CONTCAR files (.poscar/.contcar/.vasp) and DFTB+ genFormat files (.gen)
+    '''
+    # TODO: define output
+    raise NotImplementedError
+
+
 def crest():
     # TODO: define input, arguments, and output
-    # Should return instance (or list) of MDOptimizedGeometry
+    # Should return instance (or list) of CREST conformers
+    raise NotImplementedError
+
+
+def enso():
+    # TODO: define input, arguments, and output
+    # Should return instance (or list) of ENSO calculated chemical shifts
     raise NotImplementedError
 
 
@@ -13,3 +29,4 @@ def amber():
     raise NotImplementedError
 
 # TODO: add other MD methods as needed
+
