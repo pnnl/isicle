b4431b64fb3fb7198babdf703a032e411463f3d2
Sean Colby <seanmcolby@gmail.com>
2021-01-26 16:05:26 -0800

    Rename `dft` module to `qm`

--- isicle/qm.py
+++ isicle/qm.py
@@ -4,22 +4,19 @@ Assumes NWChem as default quantum chemistry package. Other options can be added 
 '''
 
 
-def optimize(self, program='NWChem', functional='b3lyp', basisset='6-31g*'):
+def dft(self, program='NWChem', functional='b3lyp', basisset='6-31g*'):
     '''
     Optimize geometry, either XYZ or PDB, using stated functional and basis set.
     Additional inputs can be grid size, optimization criteria level,
     '''
     # TODO: define input, arguments, and output
     # Should return instance (or list) of DFT
-    raise NotImplementedError
 
-
-def property():
-    '''
-    Calculates a property (shielding tensor, frequency, charges) of a given geometry
-    '''
-    # TO DOL define input, arguments, output
-    # Should return list of freq/shifts/charges of a given property
+    # save geometry to input file
+    # load/generate .nw script
+    # submit job
+    # parse result
+    # return decorated `*OptimizedGeometry` class instance
     raise NotImplementedError
 
 # TODO: add other DFT methods as needed
