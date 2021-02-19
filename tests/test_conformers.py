6235478a4ebe8e5d93881d2ae722a61fc4b1783d
Sean Colby <seanmcolby@gmail.com>
2021-02-19 14:05:51 -0800

    Add test stubs

---
+++ tests/test_conformers.py
@@ -0,0 +1,51 @@
+import pytest
+
+
+def test__method_selector():
+    raise NotImplementedError
+
+
+def test_reduce():
+    raise NotImplementedError
+
+
+def test_boltzmann():
+    raise NotImplementedError
+
+
+def test_simple_average():
+    raise NotImplementedError
+
+
+def test_lowest_energy():
+    raise NotImplementedError
+
+
+def test_threshold():
+    raise NotImplementedError
+
+
+def test__are_Geometry_instances():
+    raise NotImplementedError
+
+
+def test_build_conformational_ensemble():
+    raise NotImplementedError
+
+
+class TestConformationalEnsemble:
+
+    def test_init(self):
+        raise NotImplementedError
+
+    def test_reduce(self):
+        raise NotImplementedError
+
+    def test__apply_method(self):
+        raise NotImplementedError
+
+    def test__apply_function(self):
+        raise NotImplementedError
+
+    def test_apply(self):
+        raise NotImplementedError
