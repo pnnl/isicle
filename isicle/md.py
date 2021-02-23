1f1d88117c1dca2989cd92fbca794b31dba4e2e0
amyjystad <35346436+amyjystad@users.noreply.github.com>
2021-02-23 15:28:25 -0800

    Add MDWrapperInterface

--- isicle/md.py
+++ isicle/md.py
@@ -11,11 +11,28 @@ Files resulting from an xtb job always run in the same directory that the comman
 issued in, no matter where the input is. Can direct the .log file, but no other files.
 '''
 
-# TO DO: Have finish() save xyz contents to a callable object in memory
-# TO DO: Set up generalized temp directory to run xtb jobs in
-# TO DO: Finish job_type(), possibly combine with run?
+# TO DO : Add implicit solvation, frequency
+# TO DO : if optimize is requested along with a crest calculation, need to have the opt ignore the energy window in cycle
+# TO DO : Add in dry run option for adduct to make sure that the adduct of interest is included in crest
 
 def _program_selector(program):
+    '''
+    Selects a supported molecular dynamics program for associated simulation.
+    Currently only NWChem has been implemented.
+
+    Parameters
+    ----------
+    program : str
+        Alias for program selection (xtb).
+
+    Returns
+    -------
+    program
+        Wrapped functionality of the selected program. Must implement
+        :class:`~isicle.interfaces.MDWrapperInterface`.
+
+    '''
+
     program_map = {'xtb': XTBWrapper}
 
     if program.lower() in program_map.keys():
@@ -23,11 +40,28 @@ def _program_selector(program):
     else:
         raise ValueError('{} not a supported molecular dyanmics program.'.format(program))
 
-def md(self, path, program='xtb', template=None, **kwargs):
+def md(self, path, program='xtb', **kwargs):
     '''
-    Optimize geometry, either XYZ or PDB, using stated program.
-    Additional inputs can be grid size, optimization criteria level,
+    Optimize geometry via molecular dyanmics using supplied forcefield
+    and basis set.
+
+    Parameters
+    ----------
+    geom : :obj:`~isicle.geometry.Geometry`
+        Molecule representation.
+    program : str
+        Alias for program selection (xtb).
+    **kwargs
+        Keyword arguments to configure the simulation.
+        See :meth:`~isicle.qm.XTBWrapper.configure`.
+
+    Returns
+    -------
+    result
+        Object containing relevant outputs from the simulation.
+
     '''
+
     # Select program
     mdw = _program_selector(program)
 
@@ -46,10 +80,39 @@ def md(self, path, program='xtb', template=None, **kwargs):
     # Finish/clean up
     return mdw.finish()
 
-class XTBWrapper(Geometry, MDWrapperInterface):
-    def __init__(self,):
-        self.directory = None
-        self.commandline = None
+#check lenths block if you have parameters that are global configure at one time
+
+class XTBWrapper(MDWrapperInterface):
+    '''
+    Wrapper for xtb functionality.
+
+    Implements :class:`~isicle.interfaces.MDWrapperInterface` to ensure
+    required methods are exposed.
+
+    Attributes
+    ----------
+    temp_dir : str
+        Path to temporary directory used for simulation.
+    task_map : dict
+        Alias mapper for supported molecular dynamic presets. Thses include
+        "optimize", "crest", "nmr", "protonate", "deprtonate", and "tautomer".
+    geom : :obj:`isicle.geometry.Geometry`
+        Internal molecule representation.
+    fmt : str
+        File extension indicator.
+    job_list : str
+        List of commands for simulation.
+
+    '''
+
+    def __init__(self):
+        '''
+        Initialize :obj:`MDChemWrapper` instance.
+
+        Creates temporary directory for intermediate files, establishes aliases
+        for preconfigured tasks.
+
+        '''
         self.temp_dir = tempfile.TemporaryDirectory()
         self.task_map = {'optimize': self._job_type_optimize,
                          'crest': self._job_type_crest,
@@ -57,6 +120,54 @@ class XTBWrapper(Geometry, MDWrapperInterface):
                          'protonate': self._job_type_protonate,
                          'deprotonate': self._job_type_deprotonate,
                          'tautomer': self._job_type_tautomer}
+
+    def set_geometry(self, geom):
+        '''
+        Set :obj:`~isicle.geometry.Geometry` instance for simulation.
+
+        Parameters
+        ----------
+        geom : :obj:`~isicle.geometry.Geometry`
+            Molecule representation.
+
+        '''
+
+        # Assign geometry
+        self.geom = geom
+
+        # Extract filename
+        self.geom.basename = os.path.splitext(os.path.basename(self.geom.path))[0]
+
+    def save_geometry(self, fmt='xyz'):
+        '''
+        Save internal :obj:`~isicle.geometry.Geometry` representation to file.
+
+        Parameters
+        ----------
+        fmt : str
+            Filetype used by xtb. Must be "xyz", "smi", ".inchi", ".mol", ".xyz",
+            ".pdb", ".pkl".
+
+        '''
+
+        # Path operations
+        self.fmt = fmt.lower()
+        outfile = os.path.join(self.temp_dir.name,
+                               '{}.{}'.format(self.geom.basename,
+                                              self.fmt.lower()))
+
+        # Workaround for xyz input
+        # See `isicle.geometry.load_xyz`
+        if self.geom.filetype == '.xyz':
+            if self.fmt != 'xyz':
+                raise TypeError('Input .xyz files cannot be converted.')
+
+            with open(outfile, 'w') as f:
+                f.write('\n'.join(self.geom.contents))
+            return
+
+        # All other formats
+        self.geom.save(outfile)
 
     def load_geometry(self, path):
         # Workaround for xyz input
@@ -86,8 +197,6 @@ class XTBWrapper(Geometry, MDWrapperInterface):
         path : str
             Path to input file
             Prefered format is mol or pdb
-        directory : str
-            Directory where job will be run
         method : str 
             GFN forcefield for the optimization
             Default: gff
@@ -96,94 +205,106 @@ class XTBWrapper(Geometry, MDWrapperInterface):
             Optimization convergence level
             Default : normal
             Supported : crude, sloppy, loose, lax, normal, tight, vtight extreme
-            Format to save this molecule in. If None, determined from given
-            path's extension. If .pkl. pickles this full object.
-            Default: None.
-            Supported formats: .smi (SMILES), .inchi (InChI), .mol, .xyz,
-            .pdb, .pkl.
+    
         Returns
         -------
         xyz file
         '''
 
-        self.directory = self.filename
-        self.commandline = 'xtb {input} --opt {optlevel} --{method}'
+        return 'xtb {self.geom} --opt {optlevel} --{method} --scratch'
 
-    def _job_type_crest(self, input, forcefield='gff', optlevel='vtight', ewin=1):
-        '''
-        Runs Conformer Rotamer Ensemble Sampling Tool
-        Parameters
-        ----------
-        path : str
-            Path to input file
-        directory : str
-            Directory where job will be run
-        method : str 
-            GFN forcefield for the optimization
-            Default: gff
-            Supported forcefields: gfn2, gfn1, gff
-        optlevel : str
-            Optimization convergence level
-            Default : vtight
-            Supported : crude, sloppy, loose, lax, normal, tight, vtight extreme
-        ewin : int
-            Set the energy threshold to int kcal/mol
-            Default : 6 kcal/mol
-        Returns
-        -------
-        xyz file(s)  
-        '''
+    def _job_type_crest(self, input, forcefield='gff', optlevel='vtight', ewin=6):
 
-        self.directory = 'crest/' + self.geom_basename
-        self.commandline = ''
-
-        return 
+        return 'crest {self.geom} --ewin {ewin} --optlevel {optlevel} -{forcefield} --scratch'
 
     def _job_type_nmr(self, input):
         # self.directory = 'xtb_nmr/'
         # TO DO: get protocol for enso/anmr
         raise NotImplementedError
 
-    def _job_type_protonate(self, input, ion='H+'):
-        return ['protonate/', 'crest {input} -protonate --swel {ion}']
+    def _job_type_protonate(self, input, ion='H+', ewin=6, dryrun=False):
 
-    def _job_type_deprotonate(self, input):
-        return ['deprotonate/', 'crest {input} -deprotonate']
+        return 'crest {self.geom} -protonate --swel {ion} --ewin {ewin} --scratch'
 
-    def _job_type_tautomer(self, input):
-        self.directory = 'tautomer/'
-        self.commandline = 'crest {input}'
+    def _job_type_deprotonate(self, input, ewin=6):
 
-    def job_type(self, input, directory = self.directory, tasks='optimize', forcefield='gfn2'):
+        return 'deprotonate/', 'crest {self.geom} -deprotonate --ewin {ewin} --scratch'
+
+    def _job_type_tautomer(self, input, ewin=6):
+
+        return 'crest {self.geom} --scratch, --ewin {ewin}'
+
+
+    def job_type(self, tasks='optimize', forcefield='gff', ewin=1, ion='H+', optlevel='Normal',dryrun=False):
         '''
-        Returns the directory and command line(s) needed to run md job 
+        Set up list of xtb jobs
+
+        Parameters
+        ----------
+        tasks : str or list of str
+            Tasks text.
+        forcefield : str ot lisst of str
+            Forcefield selection. Supply globally or per task.
+        ewin : int or list of int
+            Energy window for crest calculation. 
+        ion : str 
+            Ion for protomer calculation.
+        optlevel : str or list of str
+            Set optimization level. Supply globally or per task. 
+
         '''
         
         # Cast to list safely
         tasks = safelist(tasks)
+        optlevel = safelist(optlevel)
         forcefield = safelist(forcefield)
+        energywindow = safelist(energywindow)
 
-
-        subprocess.run('mkdir {directory}')
         job_list =[]
 
-        for task, f, e in zip(tasks, cycle(forcefield)):
-            # TODO: finish this
-            config += self.task_map[task](functional=f, ewin=)
+
+        # Check lengths
+        if not ((len(tasks) == len(forcefield)) or (len(functional) == 1)):
+            raise ValueError('Forcefield must be assigned globally or per'
+                             'task.')
+        if not ((len(tasks) == len(optlevel)) or (len(optlevel) == 1)):
+            raise ValueError('Optimization level must be assigned globally or per'
+                             'task.')
+        if 'optimize' in tasks:   
+            if not ((len(tasks) == len(ewin)) or (len(ewin) == 1)):
+                raise ValueError('Energy window must be assigned globally or per'
+                                 'task.')
+
+        for task, f, o, e in zip(tasks, cycle(forcefield), cycle(optlevel), cycle(ewin)):
+            job_list.append(self.task_map[task](forcefield=f, 
+                                                ewin=ewin,
+                                                optlevel=o,
+                                                ion=ion))
+
+        self.job_list = job_list
+
+        return self.job_list
 
 
-    def run(self, opt=False, crest=False, enso=False):
+    def run(self, keep_files=True, path=None):
         '''
         subprocess.run change to working directory then runs job from command line
-        loop through an array 
-        '''
-        subprocess.run()
 
+        iterate jobs from job_type. have new temp_dir for each job?
+
+        '''
+
+        os.chdir(self.temp_dir.name)
+
+        for i in self.job_list:
+            subprocess.run(i)
 
 
     def finish(self, keep_files=True, path=None):
         '''
-        Saves
+        Save XTB output files and clean temporary direc
+
+        Parameters 
         -------
         opt: xtbopt.xyz
         conformer: crest_conformers.xyz and crest.energies, or crest_best.xyz
@@ -191,7 +312,10 @@ class XTBWrapper(Geometry, MDWrapperInterface):
         protonate: protomers.xyz
         deprotonate: deprotonated.xyz
         tautomers: tautomers.xyz
+
         '''
+
+        #TO DO : Option if the file isn't abailale? I.e. you didn't run that job or the job failed.
 
         if keep_files is True:
             import shutil
@@ -201,12 +325,18 @@ class XTBWrapper(Geometry, MDWrapperInterface):
                 raise ValueError('Must supply `path`.') 
             else:
                 # TODO: anything else to keep?
-                shutil.copy2(os.path.join(self.temp_dir.name, self.geom.basename + 'xtbopt.xyz'), path)
-                shutil.copy2(os.path.join(self.temp_dir.name, self.geom.basename + 'crest.energies'), path)
-                shutil.copy2(os.path.join(self.temp_dir.name, self.geom.basename + 'crest_conformers.xyz'), path)
-                shutil.copy2(os.path.join(self.temp_dir.name, self.geom.basename + 'crest_best.xyz'), path)
-                shutil.copy2(os.path.join(self.temp_dir.name, self.geom.basename + 'protomers.xyz'), path)
-                shutil.copy2(os.path.join(self.temp_dir.name, self.geom.basename + 'deprotonated.xyz'), path)
+                shutil.copy2(os.path.join(self.temp_dir.name, 
+                                          'xtbopt.xyz'), path)
+                shutil.copy2(os.path.join(self.temp_dir.name, 
+                                          'crest.energies'), path)
+                shutil.copy2(os.path.join(self.temp_dir.name, 
+                                          'crest_conformers.xyz'), path)
+                shutil.copy2(os.path.join(self.temp_dir.name, 
+                                          'crest_best.xyz'), path)
+                shutil.copy2(os.path.join(self.temp_dir.name, 
+                                          'protomers.xyz'), path)
+                shutil.copy2(os.path.join(self.temp_dir.name, 
+                                          'deprotonated.xyz'), path)
 
                 geoms = glob.glob(os.path.join(self.temp_dir.name, '*.{}'.format(self.fmt)))
                 [shutil.copy2(x, path) for x in geoms]
@@ -214,14 +344,10 @@ class XTBWrapper(Geometry, MDWrapperInterface):
         # Remove temporary files
         self.temp_dir.cleanup()
 
-
-class AMBERWrapper(Geometry, MDWrapperInterface)
-
-
-    def amber():
-        # Don't work on this one yet
-        # TODO: define input, arguments, and output
-        # Should return instance (or list) of MDOptimizedGeometry
-        raise NotImplementedError
+def amber():
+    # Don't work on this one yet
+    # TODO: define input, arguments, and output
+    # Should return instance (or list) of MDOptimizedGeometry
+    raise NotImplementedError
 
 # TODO: add other MD methods as needed
