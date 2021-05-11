27b3f8322628e7248f452cb06d3ab77ba1bf48f9
Nunez, Jamie R <jamie.nunez@pnnl.gov>
2021-05-11 13:58:59 -0700

    Add interfaces import

--- isicle/__init__.py
+++ isicle/__init__.py
@@ -1,6 +1,7 @@
 # from isicle import adducts
 from isicle import conformers
 from isicle import geometry
+from isicle import interfaces
 from isicle import md
 from isicle import mobility
 from isicle import parse
