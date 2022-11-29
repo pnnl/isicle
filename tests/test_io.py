8bf169b3b90ca18e775ca837e4cb93f80d69fff7
Sean Colby <sean.colby@pnnl.gov>
2022-11-29 12:52:41 -0800

    Adds test stubs

---
+++ tests/test_io.py
@@ -0,0 +1,82 @@
+import isicle
+import pytest
+
+
+def test_load_xyz():
+    raise NotImplementedError
+
+
+def test_load_mol():
+    raise NotImplementedError
+
+
+def test_load_mol2():
+    raise NotImplementedError
+
+
+def test_load_pdb():
+    raise NotImplementedError
+
+
+def test_load_smiles():
+    raise NotImplementedError
+
+
+def test_load_inchi():
+    raise NotImplementedError
+
+
+def test_load_smarts():
+    raise NotImplementedError
+
+
+def test_load_pickle():
+    raise NotImplementedError
+
+
+def test_load():
+    raise NotImplementedError
+
+
+def test_save_xyz():
+    raise NotImplementedError
+
+
+def test_save_pickle():
+    raise NotImplementedError
+
+
+def test_save_mfj():
+    raise NotImplementedError
+
+
+def test_save_():
+    raise NotImplementedError
+
+
+def test_save_smiles():
+    raise NotImplementedError
+
+
+def test_save_inchi():
+    raise NotImplementedError
+
+
+def test_save_smarts():
+    raise NotImplementedError
+
+
+def test_save_mol():
+    raise NotImplementedError
+
+
+def test_save_mol2():
+    raise NotImplementedError
+
+
+def test_save_pdb():
+    raise NotImplementedError
+
+
+def test_save():
+    raise NotImplementedError
