76cadd95318b60a1568c0cadd9669fe9ad68f031
jessbade <jessicabade01@gmail.com>
2023-05-18 13:52:26 -0700

    Fix imports

--- isicle/scripts/standardize_mol2.py
+++ isicle/scripts/standardize_mol2.py
@@ -1,5 +1,5 @@
 import argparse
-import pybel
+from openbabel import pybel
 from isicle import __version__
 
 
@@ -17,12 +17,20 @@ def standardize(path, reference, output):
     traj.write("xyz", output, True)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Standardize mol2 for use with OpenBabel')
-    parser.add_argument('mol2', help='path to .mol2 file')
-    parser.add_argument('ref', help='path to reference .mol2 file')
-    parser.add_argument('outfile', help='path to output .xyz file')
-    parser.add_argument('-v', '--version', action='version', version=__version__, help='print version and exit')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Standardize mol2 for use with OpenBabel"
+    )
+    parser.add_argument("mol2", help="path to .mol2 file")
+    parser.add_argument("ref", help="path to reference .mol2 file")
+    parser.add_argument("outfile", help="path to output .xyz file")
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=__version__,
+        help="print version and exit",
+    )
 
     args = parser.parse_args()
 
