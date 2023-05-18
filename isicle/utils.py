3153c54d3bdc66aa44a134258e344c35eb5a5bdd
jessbade <jessicabade01@gmail.com>
2023-05-18 13:53:17 -0700

    Fix imports, update formatting

--- isicle/utils.py
+++ isicle/utils.py
@@ -1,47 +1,52 @@
 import subprocess
 import pandas as pd
-import pybel
+from openbabel import pybel
 import numpy as np
 import platform
 
 
 def tail(f, lines=1):
-    return [x.strip() for x in subprocess.check_output(['tail', '-n%s' % lines, f]).decode('ascii').split('\n')]
+    return [
+        x.strip()
+        for x in subprocess.check_output(["tail", "-n%s" % lines, f])
+        .decode("ascii")
+        .split("\n")
+    ]
 
 
 def read_string(path):
-    '''Reads first line from a file.'''
+    """Reads first line from a file."""
 
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.readlines()[0].strip()
 
 
 def write_string(string, path):
-    '''Writes a string to file.'''
+    """Writes a string to file."""
 
-    with open(path, 'w') as f:
-        f.write(string.strip() + '\n')
+    with open(path, "w") as f:
+        f.write(string.strip() + "\n")
 
 
 def read_mass(path):
-    '''Reads mass from molmass.py output file.'''
+    """Reads mass from molmass.py output file."""
 
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         lines = f.readlines()
         for x in lines:
-            if 'Monoisotopic mass' in x:
+            if "Monoisotopic mass" in x:
                 return float(x.split()[-1])
 
 
 def read_pka(path):
-    '''Reads pKa from cxcalc output'''
+    """Reads pKa from cxcalc output"""
 
-    df = pd.read_csv(path, sep='\t')
+    df = pd.read_csv(path, sep="\t")
 
     # offset indices because cxcalc is 1-based
-    idx = [int(x) - 1 for x in df['atoms'].values[0].split(',')]
+    idx = [int(x) - 1 for x in df["atoms"].values[0].split(",")]
     pk = df.values[0][1:5]
-    label = ['a1', 'a2', 'b1', 'b2']
+    label = ["a1", "a2", "b1", "b2"]
 
     res = {}
     i = 0
@@ -53,7 +58,7 @@ def read_pka(path):
     return res
 
 
-def read_mol(path, fmt='mol2'):
+def read_mol(path, fmt="mol2"):
     return Mol(next(pybel.readfile(fmt, path)))
 
 
@@ -68,10 +73,10 @@ class Mol(pybel.Molecule):
         return len(self.atoms)
 
 
-def pop_atom(path, output, atom='Na'):
+def pop_atom(path, output, atom="Na"):
     to_remove = []
     to_save = []
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         lines = f.readlines()
         for i, line in enumerate(lines):
             if i == 2:
@@ -81,11 +86,11 @@ def pop_atom(path, output, atom='Na'):
                 to_save.append(line)
 
     change = len(to_remove)
-    with open(output, 'w') as f:
+    with open(output, "w") as f:
         for i, line in enumerate(lines):
             if i == 2:
                 info[0] -= change
-                f.write(' %s %s %s %s %s\n' % tuple(info))
+                f.write(" %s %s %s %s %s\n" % tuple(info))
             elif i in to_remove:
                 pass
             else:
@@ -95,57 +100,58 @@ def pop_atom(path, output, atom='Na'):
 
 
 def push_atom(path, output, idx, content):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         lines = f.readlines()
 
     info = [int(x) for x in lines[2].split()]
 
     for i, line in zip(idx, content):
-        if 'NA' in line:
+        if "NA" in line:
             parts = line.split()
-            parts[1] = 'NA'
-            parts[5] = 'Na+'
-            parts[6] = '2'
-            parts[7] = 'Na+'
-            parts[8] = '1.0000'
-            line = ' '.join(parts) + '\n'
+            parts[1] = "NA"
+            parts[5] = "Na+"
+            parts[6] = "2"
+            parts[7] = "Na+"
+            parts[8] = "1.0000"
+            line = " ".join(parts) + "\n"
         lines.insert(i + 1, line)
 
     change = len(idx)
-    with open(output, 'w') as f:
+    with open(output, "w") as f:
         for i, line in enumerate(lines):
             if i == 2:
                 info[0] += change
-                f.write(' %s %s %s %s %s\n' % tuple(info))
+                f.write(" %s %s %s %s %s\n" % tuple(info))
             else:
                 f.write(line)
 
 
 def getOS():
     system = platform.system().lower()
-    if system == 'darwin':
-        return 'osx'
+    if system == "darwin":
+        return "osx"
     return system
 
 
 def cycles(n):
-    return ['%03d' % x for x in range(1, n + 1)]
+    return ["%03d" % x for x in range(1, n + 1)]
 
 
 def frames(n):
-    return ['%03d' % x for x in range(n)]
+    return ["%03d" % x for x in range(n)]
 
 
 def smi2key(smi):
     try:
-        res = subprocess.check_output('obabel -:"%s" -oinchikey' % smi,
-                                      stderr=subprocess.STDOUT, shell=True).decode('ascii')
+        res = subprocess.check_output(
+            'obabel -:"%s" -oinchikey' % smi, stderr=subprocess.STDOUT, shell=True
+        ).decode("ascii")
     except:
         return None
 
-    res = [x.strip() for x in res.split('\n') if x is not '']
+    res = [x.strip() for x in res.split("\n") if x is not ""]
 
-    if 'molecule converted' in res[-1]:
+    if "molecule converted" in res[-1]:
         return res[-2]
 
     return None
@@ -153,14 +159,17 @@ def smi2key(smi):
 
 def inchi2key(inchi):
     try:
-        res = subprocess.check_output('echo "%s" | obabel -iinchi -oinchikey' % inchi,
-                                      stderr=subprocess.STDOUT, shell=True).decode('ascii')
+        res = subprocess.check_output(
+            'echo "%s" | obabel -iinchi -oinchikey' % inchi,
+            stderr=subprocess.STDOUT,
+            shell=True,
+        ).decode("ascii")
     except:
         return None
 
-    res = [x.strip() for x in res.split('\n') if x is not '']
+    res = [x.strip() for x in res.split("\n") if x is not ""]
 
-    if 'molecule converted' in res[-1]:
+    if "molecule converted" in res[-1]:
         return res[-2]
 
     return None
