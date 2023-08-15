87e1566b83f403590db50c727826c9f8a3d8b0fc
Sean Colby <sean.colby@pnnl.gov>
2023-08-15 13:16:49 -0700

    Update formatting, docstrings

--- isicle/conformers.py
+++ isicle/conformers.py
@@ -1,5 +1,3 @@
-import pickle
-
 import numpy as np
 import pandas as pd
 from statsmodels.stats.weightstats import DescrStatsW
@@ -23,18 +21,19 @@ def _function_selector(func):
     -------
     func
         Conformer reduction function.
-
     '''
-
+    # Mapping between names and functions
     func_map = {'boltzmann': boltzmann,
                 'simple': simple_average,
                 'lowest': lowest_energy,
                 'threshold': energy_threshold}
 
+    # Check for function by name
     if func.lower() in func_map:
         return func_map[func.lower()]
-    else:
-        raise ValueError('{} not a supported reduction function.'.format(func))
+
+    # Not a named/implemented function  
+    raise ValueError('{} not a supported reduction function.'.format(func))
 
 
 def _energy_based(func):
@@ -43,19 +42,19 @@ def _energy_based(func):
 
     Parameters
     ----------
-    func : func
+    func : function
         Conformer reduction function.
 
     Returns
     -------
     bool
         True if energy based, otherwise False.
-
     '''
-
+    # Check if among energy-based functions
     if func in [boltzmann, lowest_energy, energy_threshold]:
         return True
 
+    # Not energy based
     return False
 
 
@@ -70,16 +69,15 @@ def reduce(value, func='boltzmann', **kwargs):
     func : str
         Alias for function selection (one of "boltzmann", "simple", "lowest",
         or "threshold").
-    **kwargs
+    kwargs
         Additional keyword arguments passed to `func`.
 
     Returns
     -------
     :obj:`~pandas.DataFrame`
         Result of reduction operation.
-
     '''
-
+    # Select function
     f = _function_selector(func)
 
     # Energy-based method
@@ -110,33 +108,45 @@ def boltzmann(value, energy, index=None, atom=None):
     -------
     :obj:`~pandas.DataFrame`
         Result of reduction operation.
-
     '''
-
+    # Placeholder for index
     if index is None:
         index = np.full_like(value, -1)
+
+    # Placeholder for atom
     if atom is None:
         atom = np.full_like(value, -1)
 
+    # Initialize data frame
     df = pd.DataFrame.from_dict({'value': value,
                                  'energy': energy,
                                  'index': index,
                                  'atom': atom})
 
+    # Result container
     res = []
+
+    # Iterate over unique indices
     for name, group in df.groupby(['index', 'atom']):
+        # Compute relative delta G
         g = group['energy'] * 627.503
         mn = g.min()
         relG = g - mn
+
+        # Compute Boltzmann weighting factors
         b = np.exp(-relG / 0.5924847535)
         w = (b / b.sum()) * len(b)
 
+        # Compute weighted statistics
         ws = DescrStatsW(group['value'], weights=w, ddof=0)
 
+        # Append to container
         res.append([name[0], name[1], ws.mean, ws.std, len(group.index)])
 
+    # Initialize data frame
     res = pd.DataFrame(res, columns=['index', 'atom', 'mean', 'std', 'n'])
 
+    # Drop index if not supplied
     if np.all(index == -1):
         return res.drop(columns=['index', 'atom']).iloc[0]
 
@@ -160,22 +170,28 @@ def simple_average(value, index=None, atom=None):
     -------
     :obj:`~pandas.DataFrame`
         Result of reduction operation.
-
     '''
-
+    # Placeholder for index
     if index is None:
         index = np.full_like(value, -1)
+    
+    # Placeholder for atom
     if atom is None:
         atom = np.full_like(value, -1)
 
+    # Initialize data frame
     df = pd.DataFrame.from_dict({'value': value,
                                  'index': index,
                                  'atom': atom})
 
+    # Average per unique index
     res = df.groupby(['index', 'atom'], as_index=False).agg({'value':
                                                              ['mean', 'std', 'count']})
+    
+    # Rename columns
     res.columns = ['index', 'atom', 'mean', 'std', 'n']
 
+    # Drop indices if not supplied
     if np.all(index == -1):
         return res.drop(columns=['index', 'atom']).iloc[0]
 
@@ -201,22 +217,26 @@ def lowest_energy(value, energy, index=None, atom=None):
     -------
     :obj:`~pandas.DataFrame`
         Result of reduction operation.
-
     '''
-
+    # Placeholder for index
     if index is None:
         index = np.full_like(value, -1)
+    
+    # Placeholder for atom
     if atom is None:
         atom = np.full_like(value, -1)
 
+    # Initialize data frame
     df = pd.DataFrame.from_dict({'value': value,
                                  'energy': energy,
                                  'index': index,
                                  'atom': atom})
 
+    # Take minimum energy per unique index
     res = df.loc[df.groupby(['index', 'atom'])[
         'energy'].idxmin()]
 
+    # Drop indices if not supplied
     if np.all(index == -1):
         return res.drop(columns=['index', 'atom']).iloc[0]
 
@@ -243,25 +263,32 @@ def energy_threshold(value, energy, threshold=5, index=None, atom=None):
     -------
     :obj:`~pandas.DataFrame`
         Result of reduction operation.
-
     '''
-
+    # Placeholder for index
     if index is None:
         index = np.full_like(value, -1)
+    
+    # Placeholder for atom
     if atom is None:
         atom = np.full_like(value, -1)
 
+    # Initialize data frame
     df = pd.DataFrame.from_dict({'value': value,
                                  'energy': energy,
                                  'index': index,
                                  'atom': atom})
 
+    # Filter by energy
     df = df.loc[df['energy'] <= threshold, :]
 
+    # Aggregate
     res = df.groupby(['index', 'atom'], as_index=False).agg({'value':
                                                              ['mean', 'std', 'count']})
+    
+    # Rename columns
     res.columns = ['index', 'atom', 'mean', 'std', 'n']
 
+    # Drop indices if not supplied
     if index is None:
         return res.drop(columns=['index', 'atom']).iloc[0]
 
@@ -289,18 +316,21 @@ def transform(value, m={'H': 1.0, 'C': 1.0}, b={'H': 0.0, 'C': 0.0}, index=None,
     -------
     :obj: `~pandas.DataFrame`
         Result of transformation operation.
-
     '''
-
+    # Placeholder for index
     if index is None:
         index = np.full_like(value, -1)
+    
+    # Placeholder for atom
     if atom is None:
         atom = np.full_like(value, -1)
 
+    # Initialize data frame
     df = pd.DataFrame.from_dict({'value': value,
                                  'index': index,
                                  'atom': atom})
 
+    # Process with per-atom values
     if isinstance(m, dict):
         res = pd.DataFrame()
         for idx in m:
@@ -309,6 +339,8 @@ def transform(value, m={'H': 1.0, 'C': 1.0}, b={'H': 0.0, 'C': 0.0}, index=None,
                 lambda x: m[idx] * x + b[idx])
 
             res = pd.concat([res, part])
+    
+    # Process with global values
     else:
         res = df.copy()
         res['new_value'] = res['value'].apply(lambda x: m * x + b)
@@ -329,76 +361,14 @@ def build_conformational_ensemble(geometries):
     -------
     :obj:`~isicle.conformers.ConformationalEnsemble`
         Conformational ensemble.
-
     '''
-
     return ConformationalEnsemble(geometries)
-
-
-def load_pickle(path):
-    '''
-    Load pickled file.
-
-    Parameters
-    ----------
-    path : str
-        Path to pickle.
-
-    Returns
-    -------
-    :obj:`~isicle.conformers.ConformationalEnsemble`
-        Previously pickled :obj:`~isicle.conformers.ConformationalEnsemble`
-        instance.
-
-    Raises
-    ------
-    IOError
-        If file cannot be read.
-    TypeError
-        If file is not a supported class instance.
-
-    '''
-
-    # Load file
-    with open(path, 'rb') as f:
-        try:
-            ensemble = pickle.load(f)
-        except pickle.UnpicklingError:
-            raise IOError('Could not read file as pickle: {}'.format(path))
-
-    # Check for valid class type
-    if isinstance(ensemble, ConformationalEnsemble):
-        return ensemble
-
-    # Failure. This is not a ConformationalEnsemble instance
-    raise TypeError('Unsupported format: {}.'.format(ensemble.__class__))
-
-
-def load(path):
-    '''
-    Load file.
-
-    Parameters
-    ----------
-    path : str
-        Path to file.
-
-    Returns
-    -------
-    :obj:`~isicle.conformers.ConformationalEnsemble`
-        Previously pickled :obj:`~isicle.conformers.ConformationalEnsemble`
-        instance.
-
-    '''
-
-    return load_pickle(path)
 
 
 class ConformationalEnsemble(TypedList):
     '''
     Collection of :obj:`~isicle.geometry.Geometry`, or related subclass,
     instances.
-
     '''
 
     def __init__(self, *args):
@@ -409,9 +379,7 @@ class ConformationalEnsemble(TypedList):
         ----------
         *args
             Objects to comprise the conformational ensemble.
-
         '''
-
         super().__init__((Geometry, XYZGeometry), *args)
 
     def _check_attributes(self, attr):
@@ -427,18 +395,13 @@ class ConformationalEnsemble(TypedList):
         ------
         AttributeError
             If all members do not have `attr`.
-
         '''
-
         value = [x.get___dict__() for x in self]
         for key in safelist(attr):
             if not all(key in x for x in value):
                 raise AttributeError('"{}" not found for all conformational '
                                      'ensemble members.'.format(attr))
             value = [x.get(key) for x in value]
-
-        # if type(value[0]) is dict:
-        #     raise AttributeError('"{}" has additional keys: {}'.format(attr, value[0].keys()))
 
     def reduce(self, attr, func='boltzmann', **kwargs):
         '''
@@ -451,16 +414,15 @@ class ConformationalEnsemble(TypedList):
         func : str
             Alias for function selection (one of "boltzmann", "simple",
             "lowest", or "threshold").
-        **kwargs
+        kwargs
             Additional keyword arguments passed to `func`.
 
         Returns
         -------
         :obj:`~pandas.DataFrame`
             Result of reduction operation.
-
         '''
-
+        # Select reduction function
         f = _function_selector(func)
 
         # Check for primary attribute
@@ -516,7 +478,6 @@ class ConformationalEnsemble(TypedList):
         # Execute other method
         return f(value, index=index, atom=atom, **kwargs)
 
-    # TODO: automatically unpack multiple returned objects
     def _apply_method(self, method, **kwargs):
         '''
         Process conformational ensemble members according to supplied method.
@@ -525,16 +486,14 @@ class ConformationalEnsemble(TypedList):
         ----------
         method : str
             Method by which ensemble members will be processed.
-        **kwargs
+        kwargs
             Keyword arguments passed to `method`.
 
         Returns
         -------
         :obj:`~isicle.conformers.ConformationalEnsemble` or list
             Result of operation, type depends on `method` return type.
-
         '''
-
         # Check for attribute
         if not all(hasattr(x, method) for x in self):
             raise AttributeError('"{}" not found for all conformational '
@@ -551,7 +510,6 @@ class ConformationalEnsemble(TypedList):
         except:
             return result
 
-    # TODO: automatically unpack multiple returned objects
     def _apply_function(self, func, **kwargs):
         '''
         Process conformational ensemble members according to supplied function.
@@ -560,16 +518,14 @@ class ConformationalEnsemble(TypedList):
         ----------
         func : function
             Function by which ensemble members will be processed.
-        **kwargs
+        kwargs
             Keyword arguments passed to `func`.
 
         Returns
         -------
         :obj:`~isicle.conformers.ConformationalEnsemble` or list
             Result of operation, type depends on `func` return type.
-
         '''
-
         # Apply method to collection
         result = [func(x, **kwargs) for x in self]
 
@@ -581,7 +537,6 @@ class ConformationalEnsemble(TypedList):
         except:
             return result
 
-    # TODO: automatically unpack multiple returned objects
     def apply(self, func=None, method=None, **kwargs):
         '''
         Process conformational ensemble members according to supplied function
@@ -593,7 +548,7 @@ class ConformationalEnsemble(TypedList):
             Function by which ensemble members will be processed.
         method : str
             Method by which ensemble members will be processed.
-        **kwargs
+        kwargs
             Keyword arguments passed to `method`.
 
         Returns
@@ -605,42 +560,16 @@ class ConformationalEnsemble(TypedList):
         ------
         ValueError
             If neither `func` nor `method` is supplied.
-
         '''
+        # Apply function
         if func is not None:
             return self._apply_function(func, **kwargs)
 
+        # Apply method
         if method is not None:
             return self._apply_method(method, **kwargs)
 
         raise ValueError('Must supply `func` or `method`.')
-
-    def save_pickle(self, path):
-        '''
-        Pickle this class instance.
-
-        Parameters
-        ----------
-        path : str
-            Path to save pickle object to.
-
-        '''
-
-        with open(path, 'wb') as f:
-            pickle.dump(self, f)
-
-    def save(self, path):
-        '''
-        Save this class instance.
-
-        Parameters
-        ----------
-        path : str
-            Path to save object to.
-
-        '''
-
-        io.save(path, self)
 
     def get_structures(self):
         '''
@@ -650,9 +579,7 @@ class ConformationalEnsemble(TypedList):
         -------
         :obj:`~isicle.conformers.ConformationalEnsemble`
             Conformational ensemble.
-
         '''
-
         # Check for geom attribute
         self._check_attributes('geom')
 
