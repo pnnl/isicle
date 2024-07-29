af284f2c1ebb55aca5f6efc97d3d98d521f34990
Sean Colby <seanmcolby@gmail.com>
2024-07-29 09:25:17 -0700

    Add parse module

--- isicle/__init__.py
+++ isicle/__init__.py
@@ -1,4 +1,4 @@
-from isicle import adducts, conformers, geometry, md, mobility, qm
+from isicle import adducts, conformers, geometry, md, mobility, parse, qm
 from isicle.io import load, save
 
 __version__ = "2.0.0"
