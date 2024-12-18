2119479f774be87f6d8ff957e0e984731a52d7c4
Sean Colby <seanmcolby@gmail.com>
2024-12-18 08:54:31 -0800

    Obtain version without package import, formatting

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
-import os
 from setuptools import setup, find_packages
-import sys
 
-sys.path.append(os.path.dirname(__file__))
+with open("isicle/__init__.py") as f:
+    exec([x for x in f.readlines() if "__version__" in x][0])
 
-import isicle
-
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
-with open('requirements.txt', 'r') as f:
+with open("requirements.txt", "r") as f:
     install_requires = f.read().splitlines()
 
-pkgs = find_packages(exclude=('examples', 'docs', 'resources'))
+pkgs = find_packages(exclude=("examples", "docs", "resources"))
 
 setup(
-    name='isicle',
-    version=isicle.__version__,  # TODO: switch to versioneer.get_version() or alternate method
-    description='ISiCLE: in silico chemical library engine',
+    name="isicle",
+    version=__version__,  # TODO: switch to versioneer.get_version() or alternate method
+    description="ISiCLE: in silico chemical library engine",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Sean M. Colby',
-    author_email='sean.colby@pnnl.gov',
+    long_description_content_type="text/markdown",
+    author="Sean M. Colby",
+    author_email="sean.colby@pnnl.gov",
     license=license,
     packages=pkgs,
     python_requires=">=3.9",
@@ -33,6 +30,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Chemistry",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-    ]
+        "License :: OSI Approved :: MIT License",
+    ],
 )
